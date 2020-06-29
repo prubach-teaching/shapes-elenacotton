10327aa71cad7efb89bd2f80ae8f4c3c84dc39f5
elenacotton <61975345+elenacotton@users.noreply.github.com>
2020-06-29 11:59:58 +0200

    Create EquiTriangle.java

---
+++ src/main/java/pl/waw/sgh/shapes/EquiTriangle.java
@@ -0,0 +1,19 @@
+package pl.waw.sgh.shapes;
+
+// Insert method constructor
+public class EquilateralTriangle extends Triangle {
+    public EquilateralTriangle(double a) {
+        super(a, a, a, (a * Math.sqrt(3)) / 2);
+    }
+    
+    // Add surface calculator method
+    public double calcSurface() {
+        return ((parA * parA * Math.sqrt(3)) / 4;
+    }
+    
+    // Add perimeter calculator method
+    public double calcPerimeter() {
+        return 3 * parA;
+    }
+    
+}
