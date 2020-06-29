4fbf22af2b4f5d0281db22d0c6041ba76a8118b0
elenacotton <61975345+elenacotton@users.noreply.github.com>
2020-06-29 12:12:38 +0200

    Update Triangle.java

--- src/main/java/pl/waw/sgh/shapes/Triangle.java
+++ src/main/java/pl/waw/sgh/shapes/Triangle.java
@@ -2,14 +2,15 @@ package pl.waw.sgh.shapes;
 
 // Insert constructor method
 public class Triangle extends Shape implements PerimeterCalculation {
-    public Triangle(double a, double b, double c, double d) {
-        super(a, b, c, d);
+    public Triangle(double a, double b, double c) {
+        super(a, b, c);
     }
     
     @Override
     // Add surface calculator method
     public double calcSurface() {
-        return (parA*parD)/2;
+        Double s=(parA+parB+parC)/2;
+        return Math.sqrt(s*(s-parA)*(s-parB)*(s-parC));
     }
     
     @Override
